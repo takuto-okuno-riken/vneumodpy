13551d96db7b989058911158ccaa24b79d2c7b31
takuto-okuno-riken <66657250+takuto-okuno-riken@users.noreply.github.com>
2026-02-14 23:14:16 +0900

    fixed package version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vneumodpy",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "matplotlib", "scikit-learn", "numpy", "pandas", "scipy", "h5py", "hdf5storage", "nibabel", "statsmodels"
